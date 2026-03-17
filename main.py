6c0f3d3f61f5f44e07984dbac3e8f74a0659cc5b
VulcanWM <logmedha09@gmail.com>
2026-03-17 13:09:29 +0000

    feat: added ratelimiting

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from fastapi import FastAPI
 from queries import get_challenges, get_challenge_repo
-
+from ratelimit import is_rate_limited
+from fastapi import HTTPException
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"Hello": "World"}
+    return {"Hello": "There"}
 
 @app.get("/list_challenges")
-def list_challenges():
-    challenges = get_challenges()
-    return {"challenges": challenges}
+def list_challenges(token: str | None = None):
+    if token is None or is_rate_limited(token):
+        raise HTTPException(status_code=429, detail="rate limited")
+    else:
+        challenges = get_challenges()
+        return {"challenges": challenges}
 
 @app.get("/challenge_repo/{name}")
-def challenge_repo(name: str):
-    challenge = get_challenge_repo(name)
-    if challenge is None:
-        return {"error": "Challenge name does not exist"}
+def challenge_repo(name: str, token: str | None = None):
+    if is_rate_limited(token):
+        raise HTTPException(status_code=429, detail="rate limited")
     else:
-        return {"repo_name": "https://github.com/Recticode/" + challenge['repo_name']}
+        challenge = get_challenge_repo(name)
+        if challenge is None:
+            return {"error": "Challenge name does not exist"}
+        else:
+            return {"repo_name": "https://github.com/Recticode/" + challenge['repo_name']}
---
+++ ratelimit.py
@@ -0,0 +1,47 @@
+import redis
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+r = redis.Redis.from_url(
+    os.environ['REDIS_URI'],
+    decode_responses=True
+)
+
+def fetch_user_id_from_token(access_token: str):
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Accept": "application/vnd.github+json"
+    }
+
+    try:
+        response = requests.get("https://api.github.com/user", headers=headers, timeout=5)
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+
+    if response.status_code != 200:
+        return None
+
+    user_json = response.json()
+    return user_json.get("id")
+
+def is_rate_limited(access_token: str, limit=10, window=60):
+    user_id = r.get(f"token:{access_token}")
+
+    if user_id is None:
+        user_id = fetch_user_id_from_token(access_token=access_token)
+        if user_id:
+            r.setex(f"token:{access_token}", 3600, user_id)  # cache 1 hour
+        else:
+            return True
+
+    key = f"rate:{user_id}"
+
+    current = r.incr(key)
+    if current == 1:
+        r.expire(key, window)
+
+    return current > limit
